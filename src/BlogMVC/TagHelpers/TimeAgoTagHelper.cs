bcd9ec3a3ffce65d64b0328bf9f9d616f01b0e07
Maxime <richard.maxime9@gmail.com>
2017-01-16 22:25:29 +0100

    format date comment and post

---
+++ src/BlogMVC/TagHelpers/TimeAgoTagHelper.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+
+namespace BlogMVC.TagHelpers
+{
+    [HtmlTargetElement("timeago", TagStructure = TagStructure.NormalOrSelfClosing)]
+    [HtmlTargetElement(Attributes = "timeago")]
+    public class TimeAgoTagHelper : TagHelper
+    {
+        public ModelExpression Content { get; set; }
+        public async override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (output.TagName == "timeago")
+            {
+                output.TagName = null;
+            }
+            output.Attributes.RemoveAll("timeago");
+
+            var content = await GetContent(output);
+            var html = ToRelativeDate(DateTime.Parse(content));
+            output.Content.SetHtmlContent(html ?? "");
+        }
+
+        private async Task<string> GetContent(TagHelperOutput output)
+        {
+            if (Content == null)
+                return (await output.GetChildContentAsync()).GetContent();
+
+            return Content.Model?.ToString();
+        }
+
+        private string ToRelativeDate(DateTime dateTime)
+        {
+            var timeSpan = DateTime.Now - dateTime;
+
+            if (timeSpan <= TimeSpan.FromSeconds(60))
+                return string.Format("{0} seconds ago", timeSpan.Seconds);
+
+            if (timeSpan <= TimeSpan.FromMinutes(60))
+                return timeSpan.Minutes > 1 ? String.Format("about {0} minutes ago", timeSpan.Minutes) : "about a minute ago";
+
+            if (timeSpan <= TimeSpan.FromHours(24))
+                return timeSpan.Hours > 1 ? String.Format("about {0} hours ago", timeSpan.Hours) : "about an hour ago";
+
+            if (timeSpan <= TimeSpan.FromDays(30))
+                return timeSpan.Days > 1 ? String.Format("about {0} days ago", timeSpan.Days) : "yesterday";
+
+            if (timeSpan <= TimeSpan.FromDays(365))
+                return timeSpan.Days > 30 ? String.Format("about {0} months ago", timeSpan.Days / 30) : "about a month ago";
+
+            return timeSpan.Days > 365 ? String.Format("about {0} years ago", timeSpan.Days / 365) : "about a year ago";
+        }
+    }
+}
